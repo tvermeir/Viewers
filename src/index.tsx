dfa04aa7938547214a692be03738aed097774001
tve21 <tve21@uclive.ac.nz>
2025-07-30 12:20:51 +1200

    Removed some Console.logs that were not needed anymore

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,35 @@
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
+
 export default {
   id: 'download-image-extension',
 
-  getCommandsModule: ({ servicesManager, commandsManager, extensionManager }) => {
+  getCommandsModule: ({ servicesManager }) => {
     return {
       definitions: {
         exportZip: {
           commandFn: async () => {
-            console.log('Export zip command executed');
-
-
             const { viewportGridService, displaySetService, cornerstoneViewportService } = servicesManager.services;
 
             const viewportId = viewportGridService.getActiveViewportId();
             const viewportInfo = cornerstoneViewportService.getViewportInfo(viewportId);
 
             const canvas = viewportInfo.element.querySelector('canvas');
+            const imageBlob: Blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/jpeg', 0.8));
 
-            const imageDataUrl = canvas.toDataURL('image/jpeg', 0.8);
-            const imageBlob = await (await fetch(imageDataUrl)).blob();
             const displaySets = displaySetService.getActiveDisplaySets();
             const displaySetInstanceUID = displaySets[0]?.displaySetInstanceUID;
-            console.log('Display Set Instance UID:', displaySetInstanceUID);
-
-
             const displaySet = displaySetService.getDisplaySetByUID(displaySetInstanceUID);
 
-            console.log(displaySet);
-
             const patientName =
-              displaySet?.instances?.[0]?.PatientName?.[0]?.Alphabetic ||
-              'Unknown';
+              displaySet?.instances?.[0]?.PatientName?.[0]?.Alphabetic || 'Unknown';
             const rawDate = displaySet?.instances?.[0]?.StudyDate || 'Unknown';
 
-
-            console.log('Patient Name:', patientName);
-            console.log('Study Date:', rawDate);
             const studyDate = rawDate
               ? `${rawDate.slice(0, 4)}/${rawDate.slice(4, 6)}/${rawDate.slice(6, 8)}`
               : 'Unknown';
 
-            const metadata = {
-              patientName,
-              studyDate,
-            };
+            const metadata = { patientName, studyDate };
             const metadataJson = JSON.stringify(metadata, null, 2);
 
             const zip = new JSZip();
@@ -53,10 +38,6 @@ export default {
 
             const zipBlob = await zip.generateAsync({ type: 'blob' });
             saveAs(zipBlob, 'medical-images.zip');
-
-
-
-
           },
         },
       },
