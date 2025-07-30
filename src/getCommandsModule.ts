1f1dfe4e17e3614c67b233337774a6c2dae8d612
tve21 <tve21@uclive.ac.nz>
2025-07-30 18:20:41 +1200

    Added some documentation and updated the README.md file to include an overview of how to run my work, the steps I took to implement it, challenges I faced and assumptions I made.

--- src/getCommandsModule.ts
+++ src/getCommandsModule.ts
@@ -1,11 +1,26 @@
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
+
+/**
+ * @namespace
+ * @description OHIF Extension for downloading the acvtive viewport image and metadata as a zip file.
+ * This extension adds a custom mode with a toolbar to trigger the download.
+ */
 export default {
   id: 'download-image-extension',
-
+  /**
+   * Returns the commands module for the extension.
+   * @param {object} context - The extension context, including servicesManager, commandsManager, and extensionManager.
+   * @returns {object} Commands module definition.
+   */
   getCommandsModule: ({ servicesManager, commandsManager, extensionManager }) => {
     return {
       definitions: {
+        /**
+         * Exports the current viewport image as JPEG and metadata as JSON in a ZIP file.
+         * @async
+         * @returns {Promise<void>}
+         */
         exportZip: {
           commandFn: async () => {
             console.log('Export zip command executed');
